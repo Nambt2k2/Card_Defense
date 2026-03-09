e4c3ceb92af0c00a04b9d06d98d6014df433586c
pointB_dev <nambt2k2@gmail.com>
2026-03-09 23:09:33 +0700

    code data config cho card, tạo thử data 2 card

---
+++ Assets/_GAME/Scripts/ConfigSO/DataCardConfigSO.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Card", menuName = "DataConfigSO/DataCardConfigSO")]
+public class DataCardConfigSO : ScriptableObject {
+    public E_idCard id;
+    public E_typeCard type;
+    public E_targetCard[] target;
+    public E_elementCard element;
+    public Vector2Int size;
+    public int mana;
+    new public string name;
+    public string description;
+    public Sprite icon, iconType;
+}
---
+++ Assets/_GAME/Scripts/Enum.cs
@@ -0,0 +1,34 @@
+﻿public enum E_idCard {
+    Nhà_đực,
+    Nỏ_thần,
+    Tường_măng_tre,
+    Tháp_tằm,
+    Tháp_giao_long,
+    Lò_nung_gốm,
+    Thánh_Gióng_nhỏ,
+    Rùa_thần_pháo,
+    Mộc_tinh,
+    Đàn_chim_Lạc,
+    Chum_ma_xó,
+    Ngựa_sắt_9_hồng_mao,
+    Bão_tên_đồng,
+    Thủy_triều,
+    Hỏa_cầu,
+    Đất_mẹ_bảo_vệ,
+    Sinh_lực_mẫu_thần,
+    Ultimate,
+    Dung_hợp,
+    Thẻ_nâng_cấp,
+}
+
+public enum E_typeCard {
+    Structure, Unit, Spell, Operation,
+}
+
+public enum E_targetCard {
+    Ground, Air, Water,
+}
+
+public enum E_elementCard {
+    Kim, Mộc, Thủy, Hỏa, Thổ, 
+}
