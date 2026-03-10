94c70bf4d5167c11ada33bad91a26f8125991b01
pointB_dev <nambt2k2@gmail.com>
2026-03-10 18:54:42 +0700

    code data config cho enemy, tạo data card và enemy

--- Assets/_GAME/Scripts/ConfigSO/DataCardConfigSO.cs
+++ Assets/_GAME/Scripts/ConfigSO/DataCardConfigSO.cs
@@ -5,10 +5,11 @@ public class DataCardConfigSO : ScriptableObject {
     public E_idCard id;
     public E_typeCard type;
     public E_targetCard[] target;
-    public E_elementCard element;
+    public E_element element;
     public Vector2Int size;
     public int mana;
     new public string name;
     public string description;
-    public Sprite icon, iconType;
+    public Sprite icon;
+    public GameObject prefab;
 }
--- Assets/_GAME/Scripts/Enum.cs
+++ Assets/_GAME/Scripts/Enum.cs
@@ -1,4 +1,5 @@
-﻿public enum E_idCard {
+﻿# region CARD
+public enum E_idCard {
     Nhà_đực,
     Nỏ_thần,
     Tường_măng_tre,
@@ -28,7 +29,54 @@ public enum E_typeCard {
 public enum E_targetCard {
     Ground, Air, Water,
 }
+#endregion
 
-public enum E_elementCard {
-    Kim, Mộc, Thủy, Hỏa, Thổ, 
+#region ENEMY
+public enum E_idEnemy {
+    Chuột_tinh,
+    Quận_cú,
+    Chằn_tinh,
+    Tinh_bò_rừng,
+    Ông_ba_bị,
+    Bọ_cạp_tinh,
+    Quạ_tinh,
+    Rết_tinh,
+    Dế_chũi,
+    Chó_đội_nón_mê,
+    Tinh_đỉa,
+    Cá_sấu_5_chân,
+    Cá_bống_tinh,
+    Ma_da,
+    Ngư_tinh,
+}
+
+public enum E_areaEnemy {
+    Đồng_cỏ, Vùng_nóng, Vùng_lạnh,
+}
+
+public enum E_typeEnemy {
+    Normal, MiniBoss, Boss,
+}
+
+public enum E_typeMoveEnemy {
+    Đi_bộ, Bay, Lòng_đất, Bơi
+}
+
+public enum E_typeTaget_EnemyNotAtk {
+    None, Structure,
+}
+#endregion
+
+[System.Serializable]
+public struct S_baseStats {
+    public E_idBaseStats id;
+    public float value;
+}
+
+public enum E_idBaseStats {
+    Hp, Atk, SpeedMove, SpeedAtk, RangeAtk
+}
+
+public enum E_element {
+    Kim, Mộc, Thủy, Hỏa, Thổ, None
 }
